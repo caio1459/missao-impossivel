bbb49ec81037f73193db71baa5210d277a21986e
Caio Henrique <caiogrechic50@gmail.com>
2023-08-27 20:38:02 -0300

    Terminado a aula 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import banner from "./images/banner.jpg";
+import styles from "./styles.module.css";
+import "./styles/global.css";
+import { Header } from "./components/Header";
+// npm install
+// npm run start
 
+// https://github.com/profchines
 function App() {
   return (
     <>
-      <h1>Hello Word</h1>
-      <p>Teste</p>
-      <p>Outro teste</p>
+      <Header />
+      <img width="100%" src={banner} />
+      <h1 className={styles.title}>Hello World</h1>
+      <h1
+        style={{
+          color: "blue",
+          backgroundColor: "red",
+        }}
+      >
+        5 min pro intervalo
+      </h1>
+      <p>Primeira aula de react</p>
     </>
   );
 }
